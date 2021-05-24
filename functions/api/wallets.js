2e51e3c25592e5a93de2a38f559ba1ace51b43f8
kpose <ganihujude@gmail.com>
2021-05-24 16:05:08 +0100

    add, delete and update functions

--- functions/api/wallets.js
+++ functions/api/wallets.js
@@ -2,7 +2,7 @@ const { db } = require("../utils/admin");
 
 exports.getAllWallets = (req, res) => {
   db.collection("wallets")
-    //.orderBy("createdAt", "desc")
+    .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
       let wallets = [];
@@ -12,9 +12,71 @@ exports.getAllWallets = (req, res) => {
           title: doc.data().title,
           income: doc.data().income,
           expenses: doc.data().expenses,
+          createdAt: doc.data().createdAt,
         });
       });
       return res.json(wallets);
     })
     .catch((err) => console.log(err));
+};
+
+exports.createWallet = (request, response) => {
+  if (request.body.title.trim() === "") {
+    return response.status(400).json({ title: "Must not be empty" });
+  }
+
+  const newWallet = {
+    title: request.body.title,
+    income: {},
+    expenses: {},
+    createdAt: new Date().toISOString(),
+  };
+  db.collection("wallets")
+    .add(newWallet)
+    .then((doc) => {
+      const responseWallet = newWallet;
+      responseWallet.id = doc.id;
+      return response.json(responseWallet);
+    })
+    .catch((err) => {
+      response.status(500).json({ error: "Something went wrong" });
+      console.error(err);
+    });
+};
+
+exports.deleteWallet = (request, response) => {
+  const document = db.doc(`/wallets/${request.params.walletId}`);
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return response.status(404).json({ error: "Wallet not found" });
+      }
+      return document.delete();
+    })
+    .then(() => {
+      response.json({ message: "Delete successfull" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
+};
+
+exports.editWallet = (request, response) => {
+  if (request.body.walletId || request.body.createdAt) {
+    response.status(403).json({ message: "Not allowed to edit" });
+  }
+  let document = db.collection("wallets").doc(`${request.params.walletId}`);
+  document
+    .update(request.body)
+    .then(() => {
+      response.json({ message: "Updated successfully" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({
+        error: err.code,
+      });
+    });
 };
--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,17 @@ const functions = require("firebase-functions");
 const app = require("express")();
 const firebase = require("firebase");
 
-const { getAllWallets } = require("./api/wallets");
+const {
+  getAllWallets,
+  createWallet,
+  deleteWallet,
+  editWallet,
+} = require("./api/wallets");
 
+/* wallet routes */
 app.get("/wallets", getAllWallets);
+app.post("/createwallet", createWallet);
+app.delete("/deletewallet/:walletId", deleteWallet);
+app.put("/wallet/:walletId", editWallet);
 
 exports.api = functions.https.onRequest(app);
